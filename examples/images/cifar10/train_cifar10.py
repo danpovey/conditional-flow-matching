2395f29b0082607a0b45484a40aee933f78a3e7a
Daniel Povey <dpovey@gmail.com>
2024-05-09 21:56:10 +0800

    Remove unused function

--- examples/images/cifar10/train_cifar10.py
+++ examples/images/cifar10/train_cifar10.py
@@ -1,3 +1,4 @@
+# env to use: source /star-ly/env/py39/bin/activate
 # Inspired from https://github.com/w86763777/pytorch-ddpm/tree/master.
 
 # Authors: Kilian Fatras
@@ -8,18 +9,21 @@ import os
 import logging
 import torch
 from torch import Tensor
+from typing import Tuple
 from absl import app, flags
 from torchdyn.core import NeuralODE
 from torchvision import datasets, transforms
+from torchvision.utils import save_image
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import trange
 from utils_cifar import ema, generate_samples, infiniteloop, setup_logger
+import torch.autograd.forward_ad as fwAD
 
 from torchcfm.models.unet.unet import UNetModelWrapper
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string("output_dir", "./results_t_mod_center_1d/", help="output_directory")
+flags.DEFINE_string("output_dir", "./results_t_mod_center_1f/", help="output_directory")
 # UNet
 flags.DEFINE_integer("num_channel", 128, help="base channel of UNet")
 
@@ -52,32 +56,109 @@ def warmup_lr(step):
 
 
 
-def get_x_and_grad(t: Tensor, x0: Tensor, x1: Tensor):
-    # returns the interpolated x value and (d/dt of this).
-    # t: the time value, 0 <= t <= 1, of shape (B, 1, 1, 1)
-    # x0: the noise, of shape (B, 3, 32, 32)
-    # x1: the image, of shape (B, 3, 32, 32)
-    device = x0.device
+def get_xt_and_ut(t: Tensor, x1: Tensor) -> Tuple[Tensor, Tensor]:
+    """
+    Returns a warped version of x and the derivative of the result with
+    respect to t.  By "warped" we mean, given a random smooth
+    warp like that of a clock in a Salvador Dali painting.
 
-    # t_offset, of shape (32,), ranges from 0 to 1;
-    # this will be 0 at index 0 (which is the top, I believe)
-    # and -1 at the last index.  I believe this means we denoise
-    # the image from the top to the bottom.
-    # layout is NCHW, so t_offset is in the height axis.
-    t_offset =  (-torch.arange(32, device=device) / 31).unsqueeze(-1)
+    Args:
+       t: the time-step, between 0 and 1, where 0 means fully warped
+          and 1 means not warped at all.  Of shape (B, 1, 1, 1)
+      x1: the target image.   Note, this will be the clean image.
+          Of shape (B, 3, H, W) actually (B, 3, 32, 32).
+    Returns: (xt, dx_dt)
+    """
+
+    # https://pytorch.org/docs/stable/generated/torch.nn.functional.grid_sample.html
+
+    # First compute a random relative-warp amount.
+    (B, _, K, _) = x1.shape
+    assert x.shape[-1] == K  # we're assuming image is square.  this is not really necessary
+    assert t.shape == (B, 1, 1, 1)
+
+    x0 = torch.randn_like(x1)
+
+    # smooth out the warp, we can adjust this factor.
+    smooth_distance = 5
+    num_smooth_iters = 3
+
+    # first get a random gaussian warp amount.  the 2 is for the 2 directions,
+    # x and y.
+    warp = torch.randn(B, 2,
+                       K - num_smooth_iters * (smooth_distance - 1),
+                       K - num_smooth_iters * (smooth_distance - 1),
+                       device=x.device)
+
+    # ... and convolve it with a square of ones.
+    weight = torch.ones(2, 1, smooth_distance, smooth_distance, device=x.device)
+
+    for _ in range(num_smooth_iters):
+        warp = torch.nn.functional.conv2d(warp, weight, groups=2,
+                                          padding=(smooth_distance-1, smooth_distance-1))
+    # now warp is spatially smoothed and is close to zero near the edges.
+    assert warp.shape == (B, 2, K, K), warp.shape
+
+    warp_rms = (warp ** 2).mean().sqrt()
+    warp = warp / warp_rms
+
+    # this dictates the RMS how much the images are warped by, in pixels away from the current
+    # point.  We can change this.
 
 
-    # t_mod shape: (B, 1, 1, 32).  Values before the clamp() operation
-    # will be between -1.0 and 2.0.  At t == 1.0, all t_mod values will be 1.0,
-    # i.e. the image is fully
-    # denoised; at t=0.0, all t_mod values are 0.0 it contains only noise.
-    t_mod = (t * 2.0 + t_offset).clamp_(min=0, max=1)
-    grad_mask = torch.logical_and(t_mod > 0.0, t_mod < 1.0).to(torch.float32)
+    # this warp amount is expressed as a faction of [half the image height].
+    warp_amount = 0.05
 
-    xt = x1 * t_mod + x0 * (1 - t_mod)
-    ut = (x1 - x0) * grad_mask * 2.0
+    # The values given to grid_sample are normalized to between -1 and 1 corresponding
+    # to the e.g. top & bottom of the image,
+    # so we have to multiply warp_amount by 2 / K since the range from, say, 0 to 1 corresponds
+    # to  K / 2 pixels.
+    warp = warp * warp_amount
+    warp = warp.permute(0, 2, 3, 1)
 
-    return xt, ut
+    theta = torch.eye(3, device=x.device)[:2]
+    theta = theta.unsqueeze(0)
+    # this theta is a one-to-one affine map, saying "there is no shift or rotation"
+    grid = torch.nn.functional.affine_grid(theta, (1, 1, K, K))
+    # grid shape: (1, K, K, 2)
+    grid = grid.expand(B, K, K, 2)
+
+    # warp shape now: (B, K, K, 2).  This is the affine grid plus the
+    # random spatially-smoothed warping amount.
+
+
+    # I'm getting some not-implemented errors with fwAD.  It may be a torch version issue,
+    # so I'll just compute the derivatives with a small delta.
+    delta = 0.001
+
+    x_vals = [ ]
+    for n in [ 0, 1 ]:
+        if n == 1:
+            t = t + torch.full_like(t, delta)
+
+        delta_t = torch.full_like(t, delta) if n == 1 else 0.0
+        this_t = t + delta_t
+        x1_warped_grid = grid + (1 - this_t) * warp
+        this_x1 = torch.nn.functional.grid_sample(x1, warped_grid,
+                                                  mode='bilinear', padding_mode='reflection',
+                                                  align_corners=False)
+
+        # x0_warped_grid is like x1_warped_grid but we only keep the delta-t-related
+        # part of the warping, not the part related to the baseline t.
+        # the reason for this is to avoid any effects whereby the magnitude of x0
+        # gets smaller due to warping.  The idea is that x0 is random anyway, so we
+        # don't really need to worry about the non-changing part of the warp.
+        x0_warped_grid = grid - delta_t * warp
+        this_x0 = torch.nn.functional.grid_sample(x0, warped_grid,
+                                                  mode='bilinear', padding_mode='reflection',
+                                                  align_corners=False)
+
+        x = this_t * this_x0  + (1 - this_t) * this_x1
+        x_vals.append(x)
+
+    return x_vals[0], (x_vals[1] - x_vals[0]) / delta
+
+
 
 
 def train(argv):
@@ -162,14 +243,16 @@ def train(argv):
     with trange(FLAGS.total_steps, dynamic_ncols=True) as pbar:
         for step in pbar:
             optim.zero_grad()
-            x1 = next(datalooper).to(device)
-            x0 = torch.randn_like(x1)
-            t = torch.rand_like(get_time_shape(x1))
+            x = next(datalooper).to(device)
+            t = torch.rand_like(get_time_shape(x))
 
+            # ut is d/dt (xt)
+            xt, ut = get_xt_and_ut(t, x)
 
-            xt, ut = get_x_and_grad(t, x0, x1)
-            #xt = x1 * t + x0 * (1 - t)
-            #ut = x1 - x0
+            if step == 0:
+                save_image(x / 2 + 0.5, savedir + f"orig.png", nrow=8)
+                save_image(xt / 2 + 0.5, savedir + f"train.png", nrow=8)
+
             ut = ut.reshape(FLAGS.batch_size, -1)
 
             #t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
@@ -188,11 +271,11 @@ def train(argv):
                 logging.info(message)
                 writer.add_scalar('loss', loss, step)
 
-
             # sample and Saving the weights
             if FLAGS.save_step > 0 and step % FLAGS.save_step == 0:
                 generate_samples(net_model, FLAGS.parallel, savedir, step, net_="normal")
                 generate_samples(ema_model, FLAGS.parallel, savedir, step, net_="ema")
+
                 torch.save(
                     {
                         "net_model": net_model.state_dict(),
